9fb75fa21b34dc65382e5f6a624479d2c72ef508
Harim Choi <harim1206@gmail.com>
2018-02-20 23:24:48 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -60,7 +60,7 @@ def bulk_tweet_shortener(arr)
 
 end
 
-=begin
+
 tweetArr = [
   "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!",
   "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?",
@@ -69,4 +69,3 @@ tweetArr = [
 
 
 bulk_tweet_shortener(tweetArr)
-=end
