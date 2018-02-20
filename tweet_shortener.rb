14b7263c81a771b3143295924096cd4840f14bcf
Harim Choi <harim1206@gmail.com>
2018-02-20 23:26:19 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -55,13 +55,3 @@ def bulk_tweet_shortener(arr)
   }
 
 end
-
-
-tweetArr = [
-  "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!",
-  "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?",
-  "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
-]
-
-
-bulk_tweet_shortener(tweetArr)
