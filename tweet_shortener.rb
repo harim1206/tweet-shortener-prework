1ae38c2113c4828a549b6599ce381a5f0f69932e
Harim Choi <harim1206@gmail.com>
2018-02-20 23:27:12 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -66,3 +66,5 @@ def selective_tweet_shortener(tweet)
 end
 
 
+tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+selective_tweet_shortener(tweet)
